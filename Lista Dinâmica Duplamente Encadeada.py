c8bbd757575de6bfcb65cde515d419f1d656912c
Rafael Almeida <rafaelalmeida2909@gmail.com>
2020-12-02 18:52:07 -0300

    Adição da estrutura de Lista duplamente encadeada

---
+++ Lista Dinâmica Duplamente Encadeada.py
@@ -0,0 +1,184 @@
+class Node:
+    """Representação de um nó em Python3. Com ele é possível criar uma lista duplamente encadeada"""
+    def __init__(self, data):
+        self.data = data # Valor do nó
+        self.next = None # Próximo nó
+        self.prev = None # Nó anterior
+
+class DoublyLinkedList:
+    """Representação de uma lista dinâmica duplamente encadeada em Python3, onde cada elemento, é uma instância da classe nó"""
+    def __init__(self):
+        self.first = None # Primeiro elemento da lista 
+        self.last = None # Ultimo elemento da lista 
+        self._size = 0 # Define o nº de elementos na lista
+
+    def getItemByIndex(self, index):
+        """Método auxiliar que encontra o nó a partir do índice"""
+        if index < 0:
+            index = self._size + index
+        pointer = self.first
+        for i in range(index):
+            if pointer.next:
+                pointer = pointer.next
+            else:
+                raise IndexError("Índice fora do intervalo")
+        return pointer
+
+    def append(self, elem):
+        """Adiciona um novo nó ao final da lista"""
+        node = Node(elem)
+        if self.first: # Se não for None
+            self.last.next = node
+            node.prev = self.last
+            node.next = None
+            self.last = node
+        else:
+            self.first = self.last = node
+        self._size += 1
+
+    def remove(self, elem):
+        """Deleta a primeira aparição de um elemento na lista"""
+        if self._size == 0:
+            raise Exception("Lista Vazia")
+        index = self.index(elem)
+        del self[index]
+
+    def empty(self):
+        """Verifica se a lista está vazia"""
+        if self._size == 0:
+            return True
+        return False
+
+    def insert(self, index, elem):
+        """Insere um novo nó a partir de um índice"""
+        node = Node(elem)
+        if self._size == 0 or index >= self._size:
+            self.append(elem)
+        elif index == 0:
+            node.next = self.first
+            self.first.prev = node
+            self.first = node 
+            self._size += 1
+        elif index > 0:
+            node_next = self.getItemByIndex(index)
+            node_prev = self.getItemByIndex(index-1)
+            node.next = node_next
+            node.prev = node_prev
+            node_next.prev = node
+            node_prev.next = node
+            self._size += 1
+        else:
+            raise IndexError("Indíce inválido")
+    
+    def pop(self, index=None):
+        """Deleta o último item da lista e retorna o seu valor"""
+        if self._size == 0:
+            raise Exception("Lista vazia")
+        if index is None:
+            index = self._size-1
+        if index == self._size or abs(index) > self._size:
+            raise IndexError("Índice inválido")
+        if self._size == 1:
+            elem = self.last.data
+            self.clear()
+        else:
+            elem = self.getItemByIndex(index).data
+            del self[index]
+        return elem
+
+    def clear(self):
+        """Restaura a lista para seu ponto inicial(Vazia)"""
+        self.first = None
+        self.last = None
+        self._size = 0
+
+    def count(self, elem):
+        """Conta o número de aparições de um determinado elemento na lista"""
+        pointer = self.first
+        cont = 0
+        while(pointer != None):
+            if pointer.data == elem:
+                cont += 1
+            pointer = pointer.next
+        return cont
+
+    def index(self, elem):
+        """Retorna o índice da primeira aparição de um elemento na lista"""
+        pointer = self.first
+        cont = 0
+        while(pointer):
+            if pointer.data == elem:
+                return cont
+            else:
+                pointer = pointer.next
+                cont += 1
+        raise ValueError(f"{elem} não está na lista")
+
+    def reverse(self):
+        """Retorna a lista invertida na forma de um novo objeto"""
+        new = DoublyLinkedList()
+        pointer = self.last
+        while(pointer):
+            new.append(pointer.data)
+            pointer = pointer.prev
+        return new
+
+    def __len__(self):
+        """Retorna o tamanho da lista"""
+        return self._size
+
+    def __getitem__(self, index):
+        """Retorna o valor de um nó da lista a partir de um índice"""
+        if index == self._size or abs(index) > self._size:
+            raise IndexError("Índice inválido")
+        pointer = self.getItemByIndex(index)
+        return pointer.data
+
+    def __setitem__(self, index, elem):
+        """Atribui um novo valor a um nó a partir de um índice"""
+        if index == self._size or abs(index) > self._size:
+            raise IndexError("Índice inválido")
+        if index < 0:
+            index = self._size + index
+        pointer = self.getItemByIndex(index)
+        pointer.data = elem
+
+    def __delitem__(self, index):
+        """Remove um nó da lista a partir de um índice"""
+        if index == self._size or abs(index) > self._size:
+            raise IndexError("Índice inválido")
+        if index < 0:
+            index = self._size + index
+        if index == 0:
+            self.first = self.first.next
+            self.first.prev = None
+        elif index == self._size-1:
+            self.last = self.last.prev
+            self.last.next = None
+        else:
+            node_next = self.getItemByIndex(index+1)
+            node_prev = self.getItemByIndex(index-1)
+            node_next.prev = node_prev
+            node_prev.next = node_next
+        self._size -= 1
+
+    def __del__(self):
+        """Método destrutor"""
+        print("Objeto deletado")
+
+    def __str__(self):
+        """Método para representação da lista encadeada"""
+        rep = "\033[1;31m" + "head" + "\033[0;0m" + " -> "
+        pointer = self.first
+        while(pointer != None):
+            if pointer.next is None:
+                rep += f"{pointer.data} -> "
+                break
+            rep += f"{pointer.data} <-> "
+            pointer = pointer.next
+        rep += "\033[1;34m" + "None" + "\033[0;0m"
+        return rep
+
+    def __repr__(self):
+    	return str(self)
+    	
