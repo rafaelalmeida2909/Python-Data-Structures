44ea16191fbe2ec7cebc17545d9b18715140e932
Rafael Almeida <rafaelalmeida2909@gmail.com>
2021-01-11 20:14:52 -0300

    updated hash table

--- Hash Table.py
+++ Hash Table.py
@@ -2,34 +2,34 @@ class Position():
     """Class to represent a hash table position in Python3"""
 
     def __init__(self, key):
-        self.key = key # Save the key
-        self.data = None # Element in the table
-    
+        self.key = key  # Save the key
+        self.data = None  # Element in the table
+
     def __repr__(self):
-    	"""Represent the class (developer)"""
+        """Represent the class (developer)"""
         return str(self.key)
 
 
 class HashTable():
     """Class to represent a hash table in Python3"""
 
-    def __init__(self, maximum=101): # To a better experience, use a prime number
+    def __init__(self, maximum=101):  # To a better experience, use a prime number
         self.max = maximum  # Maximum table size
-        self._hash_table = [None] * self._max # Hash table
+        self._hash_table = [None] * self._max  # Hash table
         self._size = 0  # Number of elements in table
 
     @property
     def max(self):
-    	"""Getters to maximum table size"""
+        """Getters to maximum table size"""
         return self._max
 
     @max.setter
     def max(self, maximum):
-    	"""Setters to maximum table size"""
-        if not isinstance(size, int):
-            raise Exception("It is not a integer")
-        else:
+        """Setters to maximum table size"""
+        if isinstance(maximum, int):
             self._max = maximum
+        else:
+            raise Exception("It is not a integer")
 
     @staticmethod
     def stringProcess(string):
@@ -53,7 +53,7 @@ class HashTable():
         return index
 
     def doubleHashing(self, index, key, count):
-    	"""Apply a new hash function"""
+        """Apply a new hash function"""
         new_index = self.hashing(key, self._max-1) + 1
         return ((index + count*new_index) & int("0x7FFFFFFF", 16)) % self._max
 
@@ -76,7 +76,7 @@ class HashTable():
     def __setitem__(self, key, value):
         """Sets the value by the key; Ex: obj[key] = value"""
         if self._size == self._max:
-            return Exception("Hash table is full")
+            raise Exception("Hash table is full")
         index = self.hashing(key, self._max)
         if self._hash_table[index] != None:
             index = self.processCollisions(index, key)
@@ -90,14 +90,16 @@ class HashTable():
             raise Exception("Hash table is empty")
         index = self.hashing(key, self._max)
         if self._hash_table[index] is None:
-            raise Exception(f"The index corresponding to the key: {key}, is empty")
+            raise Exception(
+                f"The index corresponding to the key: {key}, is empty")
         else:
             if self._hash_table[index].key == key:
                 return self._hash_table[index].data
             else:
                 new_index = self.processCollisions(index, key)
                 if self._hash_table[new_index] is None:
-                    raise Exception(f"The index corresponding to the key: {key}, is empty")
+                    raise Exception(
+                        f"The index corresponding to the key: {key}, is empty")
                 return self._hash_table[new_index].data
 
     def __len__(self):
@@ -108,9 +110,14 @@ class HashTable():
         """Destructor method"""
 
     def __str__(self):
-    	"""Method to represent a hash table (user)"""
-    	return f"hash table is {self._size}/{self._max}"
+        """Method to represent a hash table (user)"""
+        return f"hash table is {self._size}/{self._max}"
 
     def __repr__(self):
-    	"""Method to represent a hash table (developer)"""
-    	return str(self)
+        """Method to represent a hash table (developer)"""
+        return str(self)
+
+
+"""
+Note: For small table sizes, the code may contain bugs
+"""
