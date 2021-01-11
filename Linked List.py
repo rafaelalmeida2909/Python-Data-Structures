925a441c2852b3707c3ea13fffc9576b6ab89b90
Rafael Almeida <rafaelalmeida2909@gmail.com>
2021-01-11 20:13:55 -0300

    updated linked list

--- Linked List.py
+++ Linked List.py
@@ -1,56 +1,56 @@
 class Node:
-    """Representação de um nó em Python3. Com ele é possível criar uma lista encadeada"""
+    """Class to represent a node in Python3"""
 
     def __init__(self, data):
-        self.data = data  # Valor do nó
-        self.next = None  # Próximo nó
+        self.data = data  # Node value
+        self.next = None  # Next node
 
 
 class LinkedList:
-    """Representação de uma lista dinâmica encadeada em Python3, onde cada elemento, é uma instância da classe nó"""
+    """Class to represent a linked list in Python3"""
 
     def __init__(self):
-        self.first = None  # Primeiro elemento da lista encadeada
-        self._size = 0  # Define o nº de elementos na lista
+        self._first = None  # First element of list
+        self._size = 0  # The size of list
 
     def getItemByIndex(self, index):
-        """Método auxiliar que encontra o nó a partir do índice"""
-        pointer = self.first
+        """Auxiliary method that returns the node by the index"""
+        pointer = self._first
         for _ in range(index):
             if pointer:
                 pointer = pointer.next
             else:
-                raise IndexError("Índice fora do intervalo")
+                raise IndexError("Index out of range")
         return pointer
 
     def processIndex(self, index):
-        """Método auxiliar que garante o funcionamento dos métodos com índice negativo"""
+        """Auxiliary method that helps with negative indexs"""
         if index is None:
             index = self._size - 1
         elif index == self._size or abs(index) > self._size:
-            raise IndexError("Índice inválido")
+            raise IndexError("Index out of range")
         if index < 0:
             index = self._size + index
         return index
 
     def append(self, elem):
-        """Adiciona um novo nó ao final da lista"""
-        if self.first:  # Se não for None
-            pointer = self.first
+        """Append a new element in the end of list"""
+        if self._first:  # if is not None
+            pointer = self._first
             while(pointer.next):
                 pointer = pointer.next
             pointer.next = Node(elem)
         else:
-            self.first = Node(elem)
+            self._first = Node(elem)
         self._size += 1
 
     def remove(self, elem):
-        """Deleta a primeira aparição de um elemento na lista"""
+        """Removes the first occurrence of the element from the list"""
         if self._size == 0:
-            raise Exception("Lista Vazia")
+            raise Exception("Empty list")
         index = self.index(elem)
         if index == 0:
-            self.first = self.first.next
+            self._first = self._first.next
             self._size -= 1
         else:
             pointer = self.getItemByIndex(index-1)
@@ -58,13 +58,13 @@ class LinkedList:
             self._size -= 1
 
     def empty(self):
-        """Verifica se a lista está vazia"""
+        """Returns true if the stack is empty, otherwise, it returns false"""
         if self._size == 0:
             return True
         return False
 
     def insert(self, index, elem):
-        """Insere um novo nó a partir de um índice"""
+        """Inserts a new element by index"""
         if index < 0 and abs(index) > self._size:
             index = 0
         elif index < 0:
@@ -72,7 +72,7 @@ class LinkedList:
         if index == 0:
             pointer = self.getItemByIndex(index)
             aux = Node(elem)
-            aux.next, self.first = pointer, aux
+            aux.next, self._first = pointer, aux
             self._size += 1
         elif index < self._size:
             pointer = self.getItemByIndex(index-1)
@@ -83,13 +83,13 @@ class LinkedList:
             self.append(elem)
 
     def pop(self, index=None):
-        """Deleta um item da lista, baseado em seu índice e retorna o seu valor"""
+        """Removes and returns the last element from the list"""
         if self._size == 0:
-            raise Exception("Lista vazia")
+            raise Exception("Empty list")
         index = self.processIndex(index)
         if index == 0:
-            elem = self.first.data
-            self.first = self.first.next
+            elem = self._first.data
+            self._first = self._first.next
             self._size -= 1
             return elem
         pointer = self.getItemByIndex(index-1)
@@ -99,13 +99,13 @@ class LinkedList:
         return elem
 
     def clear(self):
-        """Restaura a lista para seu ponto inicial(Vazia)"""
-        self.first = None
+        """Restores the list to its starting point (Empty)"""
+        self._first = None
         self._size = 0
 
     def count(self, elem):
-        """Conta o número de aparições de um determinado elemento na lista"""
-        pointer = self.first
+        """Returns the number of elements with the specified value"""
+        pointer = self._first
         cont = 0
         while(pointer != None):
             if pointer.data == elem:
@@ -114,8 +114,8 @@ class LinkedList:
         return cont
 
     def index(self, elem):
-        """Retorna o índice da primeira aparição de um elemento"""
-        pointer = self.first
+        """Returns the index of specified element"""
+        pointer = self._first
         cont = 0
         while(pointer):
             if pointer.data == elem:
@@ -123,58 +123,58 @@ class LinkedList:
             else:
                 pointer = pointer.next
                 cont += 1
-        raise ValueError(f"{elem} não está na lista")
+        raise ValueError(f"{elem} not in list")
 
     def reverse(self):
-        """Inverte lista original"""
+        """Reverses the original list"""
         if self._size == 0:
-            raise IndexError("Lista vazia")
+            raise IndexError("Empty list")
         for i in range(self._size-1, -1, -1):
             self.append(self.pop(i))
 
     def createReverse(self):
-        """Cria lista invertida em novo objeto"""
+        """Creates and returns a reversed new list"""
         if self._size == 0:
-            raise IndexError("Lista vazia")
+            raise IndexError("Empty list")
         new = LinkedList()
         for i in range(self._size-1, -1, -1):
             new.append(self[i])
         return new
 
     def __len__(self):
-        """Retorna o tamanho da lista"""
+        """Returns the size of list; Ex: len(obj)"""
         return self._size
 
     def __getitem__(self, index):
-        """Retorna o valor de um nó da lista a partir de um índice"""
+        """Returns an element that corresponding to the index; Ex: obj[index]"""
         index = self.processIndex(index)
         pointer = self.getItemByIndex(index)
         return pointer.data
 
     def __setitem__(self, index, elem):
-        """Atribui um novo valor a um nó a partir de um índice"""
+        """Sets the value by the index; Ex: obj[index] = value"""
         index = self.processIndex(index)
         pointer = self.getItemByIndex(index)
         pointer.data = elem
 
     def __delitem__(self, index):
-        """Remove um nó da lista a partir de um índice"""
+        """Removes an element that corresponding to the index; Ex: obj[index]"""
         index = self.processIndex(index)
         if index == 0:
             pointer = self.getItemByIndex(index)
-            self.first = pointer.next
+            self._first = pointer.next
         else:
             pointer = self.getItemByIndex(index-1)
             pointer.next = pointer.next.next
         self._size -= 1
 
     def __del__(self):
-        """Método destrutor invocado sempre que o código é finalizado"""
+        """Destructor method"""
 
     def __str__(self):
-        """Método para representação da lista encadeada"""
+        """Method to represent a linked list (user)"""
         rep = "\033[1;31m" + "head" + "\033[0;0m" + " -> "
-        pointer = self.first
+        pointer = self._first
         while(pointer != None):
             rep += f"{pointer.data} -> "
             if pointer.next is None:
@@ -184,4 +184,5 @@ class LinkedList:
         return rep
 
     def __repr__(self):
+        """Method to represent a linked list (developer)"""
         return str(self)
