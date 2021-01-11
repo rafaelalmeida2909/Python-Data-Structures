f27d6f092fe7b36e676db8b6e77babe94eb74a5d
Rafael Almeida <rafaelalmeida2909@gmail.com>
2021-01-11 20:15:02 -0300

    updated linked queue

--- Linked Queue.py
+++ Linked Queue.py
@@ -1,21 +1,21 @@
 class Node:
-    """Representação de um nó em Python3. Com ele é possível criar uma fila encadeada"""
+    """Class to represent a node in Python3"""
 
     def __init__(self, data):
-        self.data = data  # Valor do nó
-        self.next = None  # Próximo nó
+        self.data = data  # Node value
+        self.next = None  # Next node
 
 
 class LinkedQueue:
-    """Representação de um estrutura de fila dinâmica encadeada em Python3 (sem prioridade)"""
+    """Class to represent a Linked Queue(without priority) in Python3"""
 
     def __init__(self):
-        self._front = None  # Define o elemento que deverá sair primeiro da fila
-        self._back = None  # Define o último elemento da fila
-        self._size = 0  # Atributo privado que diz o nº de elementos na fila
+        self._front = None  # The first element of queue
+        self._back = None  # The last element of queue
+        self._size = 0  # The size of queue
 
     def enqueue(self, elem):
-        """Adiciona um elemento ao fim da fila"""
+        """Insert an element at the end of the queue"""
         if self._size == 0:
             aux = Node(elem)
             self._front = aux
@@ -28,41 +28,41 @@ class LinkedQueue:
         self._size += 1
 
     def dequeue(self):
-        """Retira o primeiro elemento da fila"""
+        """Removes and returns the first element from the queue"""
         if self._size == 0:
-            raise Exception("Fila vazia")
+            raise Exception("Empty queue")
         elem = self._front.data
         self._front = self._front.next
         self._size -= 1
         return elem
 
     def length(self):
-        """Retorna a quantidade de elementos na fila"""
+        """Returns the size of queue"""
         return self._size
 
     def first(self):
-        """Retorna o valor do primeiro elemento da fila"""
+        """Returns the first element from queue"""
         if self._size == 0:
-            raise Exception("Fila vazia")
-        return self._front
+            raise Exception("Empty queue")
+        return self._front.data
 
     def last(self):
-        """Retorna o valor do último elemento da fila"""
+        """Returns the last element from queue"""
         if self._size == 0:
-            raise Exception("Fila vazia")
-        return self._back
+            raise Exception("Empty queue")
+        return self._back.data
 
     def empty(self):
-        """Verifica se a fila está ou não vazia"""
+        """Verifys if the queue is empty"""
         if self._size == 0:
             return True
         return False
 
     def __del__(self):
-        """Método destrutor"""
+        """Destructor method"""
 
     def __str__(self):
-        """Método para representação da fila encadeada"""
+        """Method for representing the linked queue (user)"""
         rep = "\033[1;31m" + "first" + "\033[0;0m" + " -> "
         pointer = self._front
         while(pointer != None):
@@ -74,4 +74,5 @@ class LinkedQueue:
         return rep
 
     def __repr__(self):
+        """Method for representing the linked queue (developer)"""
         return str(self)
