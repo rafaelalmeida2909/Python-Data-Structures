ac89851bea59cf26ba8fa74cc5b4a1076c20da5b
Rafael Almeida <rafaelalmeida2909@gmail.com>
2021-01-11 20:14:34 -0300

    updated circular queue

--- Circular Queue.py
+++ Circular Queue.py
@@ -1,81 +1,82 @@
 class Queue:
-    """Representação de um estrutura de fila estática sequencial circular em Python3 (sem prioridade)"""
+    """Class to represent a circular sequencial static in Python3 (without priority)"""
 
     def __init__(self, maximum):
-        self.max = maximum  # Tamanho máximo da fila
-        self.queue = [None] * maximum  # Fila iniciada com tamanho definido
-        self._front = 0  # Define o elemento que deverá sair primeiro da fila
-        self._size = 0  # Atributo privado que diz o nº de elementos na fila
-        self._back = 0  # Define o último elemento da fila
+        self.max = maximum  # The maximum queue size
+        self._queue = [None] * maximum  # Empty queue
+        self._front = 0  # The first element of queue
+        self._size = 0  # The size of queue
+        self._back = 0  # The last element of queue
 
     @property
     def max(self):
-        """Retorna o valor do atributo max"""
+        """Getters to maximum table size"""
         return self._max
 
     @max.setter
     def max(self, maximum):
-        """Garante que max tenha um valor inteiro"""
+        """Setters to maximum table size"""
         if isinstance(maximum, int):
             self._max = maximum
         else:
             raise Exception("Atributo deve ser um número inteiro")
 
     def enqueue(self, elem):
-        """Adiciona um elemento ao fim da fila"""
-        if self._size == self.max:  # Verifica se a fila está cheia
-            raise Exception("Fila Cheia!")
-        self.queue[self._back] = elem
-        self._back = (self._back + 1) % self.max  # Garante uma fila circular
+        """Append a new element in the end of queue"""
+        if self._size == self.max:  # Verifys if the queue is full
+            raise Exception("Full queue!")
+        self._queue[self._back] = elem
+        self._back = (self._back + 1) % self.max  # Ensures a circular queue
         self._size += 1
 
     def dequeue(self):
-        """Retira o primeiro elemento da fila"""
-        if self._size == 0:  # Verifica se a fila está vazia
-            raise Exception("Fila Vazia!")
-        elem = self.queue[self._front]
-        self.queue[self._front] = None
-        self._front = (self._front + 1) % self.max  # Garante uma fila circular
+        """Removes the first element from the queue"""
+        if self._size == 0:  # Verifys if the queue is empty
+            raise Exception("Empty queue")
+        elem = self._queue[self._front]
+        self._queue[self._front] = None
+        self._front = (self._front + 1) % self.max  # Ensures a circular queue
         self._size -= 1
         return elem
 
     def length(self):
-        """Retorna a quantidade de elementos na fila"""
+        """Returns the size of queue"""
         return self._size
 
     def first(self):
-        """Retorna o valor do primeiro elemento da fila"""
+        """Returns the first element from queue"""
         if self._size == 0:
-            raise Exception("Fila vazia")
-        return self.queue[self._front]
+            raise Exception("Empty queue")
+        return self._queue[self._front]
 
     def last(self):
-        """Retorna o valor do último elemento da fila"""
+        """Returns the last element from queue"""
         if self._size == 0:
-            raise Exception("Fila vazia")
-        return self.queue[self._back-1]
+            raise Exception("Empty queue")
+        return self._queue[self._back-1]
 
     def empty(self):
-        """Verifica se a lista está vazia"""
+        """Verifys if the queue is empty"""
         if self._size == 0:
             return True
         return False
 
     def __del__(self):
-        """Método destrutor"""
+        """Destructor method"""
 
     def __str__(self):
-        """Representa a fila excluindo os obj NoneType"""
+        """Method for representing the queue, excluding NoneType objects (user)"""
         tam = f"\033[1;32m{self.max}\033[0;0m"
         rep = f"[{tam}]\033[1;31mfirst\033[0;0m -> "
         cont = self._front
         for _ in range(self._size):
             if cont == self.max:
                 cont = 0
-            rep += f"{self.queue[cont]} -> "
+            rep += f"{self._queue[cont]} -> "
             cont += 1
         rep += "\033[1;34mNone\033[0;0m"
         return rep
 
     def __repr__(self):
+        """Method for representing the queue, excluding NoneType objects (developer)"""
         return str(self)
